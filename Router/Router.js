22a8a2f862b54b40a7fdbf48b7f3b17ba5be3caa
Madhu2201 <madhumathi2201@gmail.com>
2023-11-15 19:22:59 +0530

    third commit

--- Router/Router.js
+++ Router/Router.js
@@ -2,7 +2,6 @@ import { uploadimage,getProduct } from "../controller/Controller.js";
 import upload from "../middleware/product.middleware.js";
 import  express  from "express";
 const router=express.Router();
-// router.post('/uploads', upload.array('image',5), uploadimage);
 router.post('/uploads',uploadimage)
 router.get('/getproduct',getProduct)
 export default router;
--- controller/Controller.js
+++ controller/Controller.js
@@ -1,29 +1,6 @@
 import product from "../Model/Schema.js";
 
 
-// export const uploadimage=async(req,res)=>{
-//  try{
-//  const {title,discribtion,price}=req.body;
-// const image = req.files.map(file => file.filename);
-//  console.log(req.body); 
-//  console.log(req.file);
-//  if (!req.files || req.files.length === 0) {
-//   return res.status(400).json({ message: "No files uploaded" });
-// }
- 
-//   const product1 = new product({ image, title, discribtion, price });
-//   await product1.validate(); 
-//   await product1.save();
-//   res.status(200).json({message:"product is successfully added"})
-//  }catch(error){
-//   console.log("error",error);
-//   if (error.name === 'ValidationError') {
-//     return res.status(400).json({ message: error.message });
-//   }
-
-//   res.status(500).json({message:"product is not added"})
-//  }
-// }
 
 export const uploadimage = async (req, res) => {
   try {

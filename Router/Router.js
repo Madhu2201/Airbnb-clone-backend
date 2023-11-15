510d68fe4a3aadabe1597995d664d3f9a054dc4e
Madhu2201 <madhumathi2201@gmail.com>
2023-11-15 19:29:35 +0530

    next change

--- Router/Router.js
+++ Router/Router.js
@@ -1,5 +1,4 @@
 import { uploadimage,getProduct } from "../controller/Controller.js";
-import upload from "../middleware/product.middleware.js";
 import  express  from "express";
 const router=express.Router();
 router.post('/uploads',uploadimage)

a4b616cc0fc33234483f8d927157d9db9cca8a0e
madhumathi <madhumathi2201@gmail.com>
2024-03-16 22:08:21 +0530

    reset password commit

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -1,7 +1,13 @@
-import { login, signup, getAllUsers } from "../Controller/userController.js"
-import express from "express"
-const router=express.Router();
-router.post('/signup',signup)
-router.post('/login',login)
-router.get('/getuser',getAllUsers)
+import {
+  login,
+  signup,
+  getAllUsers,
+  resetpassword,
+} from "../Controller/userController.js";
+import express from "express";
+const router = express.Router();
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/getuser", getAllUsers);
+router.post("/resset", resetpassword);
 export default router;
